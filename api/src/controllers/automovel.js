fe48f34b474629f74d1cb9b70e2b457704c2950a
wellifabio <wellifabio@hotmail.com>
2025-10-27 13:52:06 -0300

    prisma

--- api/src/controllers/automovel.js
+++ api/src/controllers/automovel.js
@@ -1,5 +1,5 @@
-const {prismaClient} = require('@prisma/client');
-const prisma = new prismaClient();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 const read = async (req, res) => {
     automoveis = await prisma.automovel.findMany();
--- api/src/routes.js
+++ api/src/routes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-// Example route
+const Automovel = require('./controllers/automovel');
+
+// Rota raiz
 router.get('/', (req, res) => {
   res.json({titulo:'Fábrica de Automóveis 2025'});
 });
+
+// Rota para obter todos os automóveis
+router.get('/automoveis', Automovel.read);
 
 module.exports = router;
