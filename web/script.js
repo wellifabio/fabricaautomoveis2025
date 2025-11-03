a40e74f5736151abb3ba92ce50e9f3fe1738c5c6
wellifabio <wellifabio@hotmail.com>
2025-11-03 08:09:17 -0300

    areas pintadas

--- web/script.js
+++ web/script.js
@@ -1,16 +1,41 @@
 const titulo = document.querySelector('header h1');
 const uri = 'http://localhost:3000/';
+var alocacoes = [];
 
-fetch(uri)
-    .then(response => response.json())
-    .then(data => {
-        titulo.textContent = data.titulo;
-    });
-
-const main = document.querySelector('main');
-for (let i = 1; i <= 11; i++) {
-    const area = document.createElement('div');
-    area.id = `area-${i}`;
-    area.innerHTML = `<p>${i}</p>`;
-    main.appendChild(area);
+async function carregarTitulo() {
+    const response = await fetch(uri);
+    const data = await response.json();
+    titulo.textContent = data.titulo;
 }
+
+async function carregarAlocacoes() {
+    const response = await fetch(uri + 'alocacoes');
+    alocacoes = await response.json();
+    console.log(alocacoes);
+}
+
+async function montarAreas() {
+    const main = document.querySelector('main');
+    for (let i = 1; i <= 11; i++) {
+        const area = document.createElement('div');
+        area.id = `area-${i}`;
+        area.innerHTML = `<p>${i}</p>`;
+        main.appendChild(area);
+    }
+}
+
+async function inicializar() {
+    await carregarTitulo();
+    await montarAreas();
+    await carregarAlocacoes();
+    await pintarAreas();
+}
+
+async function pintarAreas(){
+    for(const alocacao of alocacoes){
+        const area = document.querySelector(`#area-${alocacao.area}`);
+        area.classList.add('alocado');
+    }
+}
+
+inicializar();
